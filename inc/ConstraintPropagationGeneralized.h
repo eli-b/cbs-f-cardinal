1ee746ef6c70ceafae725ccb3fc08836af09027c
HanZhang39 <hanzhang39@hotmail.com>
2020-04-14 16:21:02 -0700

    fix bug in generalized mutex

--- inc/ConstraintPropagationGeneralized.h
+++ inc/ConstraintPropagationGeneralized.h
@@ -3,10 +3,10 @@
 class ConstraintPropagationGeneralized: public ConstraintPropagation
 {
 protected:
-  boost::unordered_set<node_pair> general_mutexes;
+  boost::unordered_set<edge_pair> general_mutexes;
 
-  bool has_generalized_node_mutex(MDDNode* a, MDDNode* b);
-  void add_generalized_node_mutex(MDDNode* node_a, MDDNode* node_b);
+  bool has_generalized_mutex(node_pair a, node_pair b);
+  void add_generalized_mutex(node_pair node_a, node_pair node_b);
 
 public:
   void fwd_mutex_prop();
--- src/ConstraintPropagationGeneralized.cpp
+++ src/ConstraintPropagationGeneralized.cpp
@@ -6,18 +6,26 @@ void ConstraintPropagationGeneralized::fwd_mutex_prop(){
 
     MDD nodes a and b (a.t > b.t) is is mutex if all of a's parent is mutex with b.
   */
-  std::vector<boost::unordered_set<edge_pair> > to_check(max(mdd0->levels.size(), mdd1->levels.size()));
+  std::vector<boost::unordered_set<edge_pair> > to_check(2 * max(mdd0->levels.size(), mdd1->levels.size()));
 
-  unordered_map<MDDNode*, list<MDDNode*>> generalMutexes;
+  unordered_map<node_pair, list<node_pair>> generalMutexes;
 
   for (const auto & mutex: fwd_mutexes){
     int l = mutex.first.first->level;
-    to_check[l].insert(mutex);
+    if ( is_edge_mutex(mutex) ){
+      to_check[2 * l + 1].insert(mutex);
+    }
+    else{
+      to_check[2 * l].insert(mutex);
+    }
   }
 
   for (int i = 0; i < to_check.size(); i ++){
     for (auto & mutex: to_check[i]){
+      generalMutexes[mutex.first].push_back(mutex.second);
+      generalMutexes[mutex.second].push_back(mutex.first);
       if (is_edge_mutex(mutex)){
+
         auto node_to_1 = mutex.first.second;
         auto node_to_2 = mutex.second.second;
 
@@ -32,9 +40,7 @@ void ConstraintPropagationGeneralized::fwd_mutex_prop(){
                                         std::make_pair(node_to_2, nullptr));
 
         fwd_mutexes.insert(new_mutex);
-        generalMutexes[node_to_1].push_back(node_to_2);
-        generalMutexes[node_to_2].push_back(node_to_1);
-        assert(i + 1 == node_to_1->level);
+        assert(i + 1 == 2 * node_to_1->level);
         to_check[i + 1].insert(new_mutex);
 
       }else{
@@ -42,102 +48,110 @@ void ConstraintPropagationGeneralized::fwd_mutex_prop(){
         auto node_a = mutex.first.first;
         auto node_b = mutex.second.first;
 
-        generalMutexes[node_a].push_back(node_b);
-        generalMutexes[node_b].push_back(node_a);
         // Check their child
 
         for (auto node_a_ch: node_a->children){
           for (auto node_b_ch: node_b->children){
             add_fwd_edge_mutex(node_a, node_a_ch, node_b, node_b_ch);
-            if (has_fwd_mutex(node_a_ch, node_b_ch)){
-              continue;
-            }
-            if (!should_be_fwd_mutexed(node_a_ch, node_b_ch)){
-              continue;
-            }
-
-            auto new_mutex = std::make_pair(std::make_pair(node_a_ch, nullptr),
-                                            std::make_pair(node_b_ch, nullptr));
-
-            fwd_mutexes.insert(new_mutex);
-            generalMutexes[node_a_ch].push_back(node_b_ch);
-            generalMutexes[node_b_ch].push_back(node_a_ch);
-            assert(i + 1 == node_a_ch->level);
-            to_check[i + 1].insert(new_mutex);
+            to_check[i + 1].insert({{node_a, node_a_ch}, { node_b, node_b_ch}});
           }
         }
       }
     }
   }
 
-  for (int l = 1; l < mdd0->levels.size(); l++){
+  for (int l = 0; l < mdd0->levels.size(); l++){
     for (MDDNode* node: mdd0->levels[l]){
       int i = 0;
-      unordered_set<MDDNode*> mutexes;
+      unordered_set< node_pair> mutexes;
       for (auto parent: node->parents){
+        node_pair e = {parent, node};
         if (i == 0){
-          mutexes = unordered_set<MDDNode*>(generalMutexes[parent].begin(), generalMutexes[parent].end());
+          mutexes = unordered_set<node_pair>(generalMutexes[e].begin(), generalMutexes[e].end());
           i ++;
         }else{
           if ( mutexes.size() == 0 ){continue;}
 
-          unordered_set<MDDNode*> tmp_mutexes;
-          for (MDDNode* mutex_node: generalMutexes[parent]){
-            if (mutexes.find(mutex_node) != mutexes.end()){
-              tmp_mutexes.insert(mutex_node);
+          unordered_set<node_pair> tmp_mutexes;
+          for (node_pair mutex: generalMutexes[e]){
+            if (mutexes.find(mutex) != mutexes.end()){
+              tmp_mutexes.insert(mutex);
             }
           }
           mutexes = tmp_mutexes;
         }
       }
-      for (MDDNode* mutex_node: mutexes){
-        generalMutexes[node].push_back(mutex_node);
+      for (node_pair mutex_node: mutexes){
+        generalMutexes[{node, nullptr}].push_back(mutex_node);
+        add_generalized_mutex({node, nullptr}, mutex_node);
+      }
+      for (MDDNode* ch_node: node->children){
+        node_pair e = {node, ch_node};
+        for (node_pair m : generalMutexes[{node, nullptr}])
+          {
+            generalMutexes[e].push_back(m);
+            add_generalized_mutex(e, m);
+          }
       }
     }
   }
-  for (int l = 1; l < mdd1->levels.size(); l++){
+  
+  for (int l = 0; l < mdd1->levels.size(); l++){
     for (MDDNode* node: mdd1->levels[l]){
       int i = 0;
-      unordered_set<MDDNode*> mutexes;
+      unordered_set< node_pair> mutexes;
       for (auto parent: node->parents){
+        node_pair e = {parent, node};
         if (i == 0){
-          mutexes = unordered_set<MDDNode*>(generalMutexes[parent].begin(), generalMutexes[parent].end());
+          mutexes = unordered_set<node_pair>(generalMutexes[e].begin(), generalMutexes[e].end());
           i ++;
         }else{
           if ( mutexes.size() == 0 ){continue;}
 
-          unordered_set<MDDNode*> tmp_mutexes;
-          for (MDDNode* mutex_node: generalMutexes[parent]){
-            if (mutexes.find(mutex_node) != mutexes.end()){
-              tmp_mutexes.insert(mutex_node);
+          unordered_set<node_pair> tmp_mutexes;
+          for (node_pair mutex: generalMutexes[e]){
+            if (mutexes.find(mutex) != mutexes.end()){
+              tmp_mutexes.insert(mutex);
             }
           }
           mutexes = tmp_mutexes;
         }
       }
-      for (MDDNode* mutex_node: mutexes){
-        generalMutexes[mutex_node].push_back(node);
+      for (node_pair mutex_node: mutexes){
+        generalMutexes[{node, nullptr}].push_back(mutex_node);
+        add_generalized_mutex(mutex_node, {node, nullptr});
       }
-    }
-  }
-  for (const auto & map_entry: generalMutexes){
-    MDDNode* node = map_entry.first;
-    for (MDDNode* mutex : map_entry.second){
-      if (node->level > mutex->level){
-        add_generalized_node_mutex(node, mutex);
+      for (MDDNode* ch_node: node->children){
+        node_pair e = {node, ch_node};
+        for (node_pair m : generalMutexes[{node, nullptr}])
+          {
+          generalMutexes[e].push_back(m);
+          add_generalized_mutex(m, e);
+          }
       }
     }
   }
 
+  // for (const auto & map_entry: generalMutexes){
+  //   node_pair node = map_entry.first;
+  //   for (node_pair mutex : map_entry.second){
+  //     if (2 * node.first->level + (node.second == nullptr? 1:0) > 2 * mutex.first->level + (mutex.second == nullptr? 1:0)){
+  //       add_generalized_mutex(node, mutex);
+  //     }
+  //   }
+  // }
+  
+
+  cout  << "a";
   return;
 }
 
-bool ConstraintPropagationGeneralized::has_generalized_node_mutex(MDDNode* a, MDDNode* b){
+bool ConstraintPropagationGeneralized::has_generalized_mutex(node_pair a, node_pair b){
   return general_mutexes.find({a, b}) != general_mutexes.end() || general_mutexes.find({b, a}) != general_mutexes.end();
 }
 
-void ConstraintPropagationGeneralized::add_generalized_node_mutex(MDDNode* node_a, MDDNode* node_b){
-  if (has_generalized_node_mutex(node_a, node_b)){
+void ConstraintPropagationGeneralized::add_generalized_mutex(node_pair node_a, node_pair node_b){
+  if (has_generalized_mutex(node_a, node_b)){
     return;
   }
   general_mutexes.insert({node_a, node_b});
