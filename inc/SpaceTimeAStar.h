014ae041f7a508d0dbc978b8717ba16d51d0f84c
Eli Boyarski <eli@boyar.ski>
2020-05-26 00:15:59 +0300

    Clarified the logic of the equality of AStarNode objects

--- inc/SpaceTimeAStar.h
+++ inc/SpaceTimeAStar.h
@@ -53,11 +53,12 @@ public:
 	{
 		bool operator()(const AStarNode* s1, const AStarNode* s2) const
 		{
-			return (s1 == s2) || (s1 && s2 &&
-                        s1->location == s2->location &&
-                        s1->timestep == s2->timestep &&
-						s1->wait_at_goal == s2->wait_at_goal) &&
-						s1->unsatisfied_positive_constraint_sets == s2->unsatisfied_positive_constraint_sets;
+			return (s1 == s2) ||
+				   (s1 && s2 &&
+					s1->location == s2->location &&
+					s1->timestep == s2->timestep &&
+					s1->wait_at_goal == s2->wait_at_goal &&
+					s1->unsatisfied_positive_constraint_sets == s2->unsatisfied_positive_constraint_sets);
 		}
 	};
 };
